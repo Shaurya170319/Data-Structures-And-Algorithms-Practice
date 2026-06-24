11f61597e9fea3ee05a09137acbf2dd9e9bb6cbb
Shaurya170319 <Shaurya22256@gmail.com>
2026-06-24 12:27:10 +0530

    Update in day1

---
+++ DSA Practice/Program4.java
@@ -0,0 +1,31 @@
+ class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + calculator.add(a, b));
+        System.out.println("Subtraction: " + calculator.subtract(a, b));
+        System.out.println("Multiplication: " + calculator.multiply(a, b));
+        System.out.println("Division: " + calculator.divide(a, b));
+    }
+    
+}
